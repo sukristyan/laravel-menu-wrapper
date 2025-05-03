fa965ac0ed0ffb53f8a5373eb056255ef2992c06
sukristyan <masgalih320@gmail.com>
2025-05-03 23:33:23 +0800

    integrate exception & config detection

--- src/Facade/RegisterMenu.php
+++ src/Facade/RegisterMenu.php
@@ -2,33 +2,60 @@
 
 namespace Sukristyan\LaravelMenuWrapper\Facade;
 
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Facade;
+use Sukristyan\LaravelMenuWrapper\Exceptions\InvalidGroupException;
 
 class RegisterMenu extends Facade
 {
-    protected static $menus = [];
-    protected static $currentGroup = null;
+    protected static array $menus = [];
+    protected static string|null $currentGroup = null;
 
-    public static function groupping($label)
+    public static function groupping(string $label): void
     {
-        self::$currentGroup = $label;
-        self::$menus[$label] = [];
-    }
+        static::$currentGroup = $label;
 
-    public static function endGroupping()
-    {
-        self::$currentGroup = null;
-    }
-
-    public static function add($uri, $label)
-    {
-        if (self::$currentGroup) {
-            self::$menus[self::$currentGroup][$uri] = $label;
+        if (!isset(static::$menus[$label])) {
+            static::$menus[$label] = [
+                'label' => $label,
+                'children' => [],
+            ];
         }
     }
 
-    public static function all()
+    public static function endGroup(): void
     {
-        return self::$menus;
+        static::$currentGroup = null;
+    }
+
+    public static function add(Route $route, string $label): void
+    {
+        if (static::$currentGroup) {
+            static::$menus[static::$currentGroup]['children'][] = [
+                'route_name' => $route->getName(),
+                'uri' => $route->uri(),
+                'label' => $label,
+            ];
+        } else {
+            static::$menus[] = [
+                'route_name' => $route->getName(),
+                'uri' => $route->uri(),
+                'label' => $label,
+            ];
+        }
+    }
+
+    public static function all(): array
+    {
+        return static::$menus;
+    }
+
+    private static function groupAs()
+    {
+        if (!in_array($given = config('laravel-menu-wrapper.group_as'), $expect = ['item', 'key'])) {
+            throw InvalidGroupException::create($given, $expect);
+        }
+
+        return config('laravel-menu-wrapper.group_as');
     }
 }
